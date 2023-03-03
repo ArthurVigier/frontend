91957343e1f9cf9d123927c3a8448add86d58b9d
Arthur <arvigier@gmail.com>
2023-03-03 16:35:30 +0100

    d

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var express = require('express');
-var router = express.Router();
-var fetch = require('fetch')
-
 document.querySelector('https://backend-alpha-sandy.vercel.app').textContent;
   fetch('https://backend-alpha-sandy.vercel.app')
  .then(response => response.json())
